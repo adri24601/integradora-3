4c852ba2c9b5eb5f00ec3f64fa407dd7e9eecbb0
DULCE LUCIA ACOSTA SOTO <al25311856@utcj.edu.mx>
2026-07-16 18:07:34 -0600

    dulce

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,14 +34,18 @@
             label1 = new Label();
             label2 = new Label();
             pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            label4 = new Label();
+            label3 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // btnInicio
             // 
-            btnInicio.Location = new Point(455, 255);
+            btnInicio.Location = new Point(401, 637);
             btnInicio.Name = "btnInicio";
-            btnInicio.Size = new Size(228, 112);
+            btnInicio.Size = new Size(150, 58);
             btnInicio.TabIndex = 0;
             btnInicio.Text = "Inicio";
             btnInicio.UseVisualStyleBackColor = true;
@@ -49,62 +53,98 @@
             // 
             // txtUsuario
             // 
-            txtUsuario.Location = new Point(524, 89);
+            txtUsuario.Location = new Point(337, 334);
             txtUsuario.Name = "txtUsuario";
-            txtUsuario.Size = new Size(150, 31);
+            txtUsuario.Size = new Size(296, 31);
             txtUsuario.TabIndex = 1;
             // 
             // txtContrasena
             // 
-            txtContrasena.Location = new Point(524, 145);
+            txtContrasena.Location = new Point(337, 493);
             txtContrasena.Name = "txtContrasena";
             txtContrasena.PasswordChar = '*';
-            txtContrasena.Size = new Size(150, 31);
+            txtContrasena.Size = new Size(296, 31);
             txtContrasena.TabIndex = 2;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(400, 89);
+            label1.Font = new Font("Sitka Small", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(432, 281);
             label1.Name = "label1";
-            label1.Size = new Size(72, 25);
+            label1.Size = new Size(125, 35);
             label1.TabIndex = 3;
-            label1.Text = "Usuario";
+            label1.Text = "Usuario:";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(400, 151);
+            label2.Font = new Font("Sitka Small", 12F, FontStyle.Bold);
+            label2.Location = new Point(401, 432);
             label2.Name = "label2";
-            label2.Size = new Size(101, 25);
+            label2.Size = new Size(171, 35);
             label2.TabIndex = 4;
-            label2.Text = "Contraseña";
+            label2.Text = "Contraseña:";
             // 
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.IMG_20260609_WA0003;
-            pictureBox1.Location = new Point(12, 49);
+            pictureBox1.Location = new Point(199, 147);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(364, 351);
+            pictureBox1.Size = new Size(592, 454);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 5;
             pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(txtUsuario);
+            panel1.Controls.Add(txtContrasena);
+            panel1.Controls.Add(btnInicio);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(label2);
+            panel1.Location = new Point(1035, -1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(863, 1024);
+            panel1.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Bernard MT Condensed", 28F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label4.Location = new Point(294, 148);
+            label4.Name = "label4";
+            label4.Size = new Size(381, 67);
+            label4.TabIndex = 5;
+            label4.Text = "Inicio de Sesion";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Britannic Bold", 48F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.ButtonHighlight;
+            label3.Location = new Point(52, 696);
+            label3.Name = "label3";
+            label3.Size = new Size(979, 106);
+            label3.TabIndex = 7;
+            label3.Text = "Sistema De Inventario";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.FromArgb(49, 6, 158);
+            ClientSize = new Size(1898, 1024);
+            Controls.Add(label3);
+            Controls.Add(panel1);
             Controls.Add(pictureBox1);
-            Controls.Add(label2);
-            Controls.Add(label1);
-            Controls.Add(txtContrasena);
-            Controls.Add(txtUsuario);
-            Controls.Add(btnInicio);
             Name = "Form1";
-            Text = "Sesion";
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -117,5 +157,8 @@
         private Label label1;
         private Label label2;
         private PictureBox pictureBox1;
+        private Panel panel1;
+        private Label label3;
+        private Label label4;
     }
 }
